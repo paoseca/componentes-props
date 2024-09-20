e999cd7e9b8c2921db9a099bd9b21f94da9b4a02
Giovanna Fonseca <giovannafonsecasilva2004@gmail.com>
2024-09-20 13:21:36 -0300

    feat: adciona componente Cartao para exibir pedidos

---
+++ src/Cartao.js
@@ -0,0 +1,18 @@
+Cartao.js
+
+import React from 'react'
+
+const Cartao = (props) => {
+  return (
+    <div class='card'>
+        <div className="card-heater text-muted">
+            {props.cabecalho}   
+        </div>
+        <div className="card-body">
+            {props.children}
+        </div>
+    </div>
+  )
+}
+
+export default Cartao
