9bd5339a831cce739b540619744f5cfa91ddf0cc
Giovanna Fonseca <giovannafonsecasilva2004@gmail.com>
2024-09-20 13:19:22 -0300

    feat: implementa index react App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return <div className='container border p-4'>
+    <div className='row'>
+      <div className='col-12'>
+       <h1 className='display-5 text-center border-bottom mb-4'>Seus pedidos</h1>
+      </div>
+    </div>
+    <div className='row'>
+      <div className='col-sm-12 col-lg-6 col-xxl-4'>
+        <Cartao cabecalho="22/05/2022">
+          <Pedido 
+            titulo="SSD"
+            descricao="SSD 256Gb"
+            icone="fa-solid fa-hard-drive fa-2x"/>
+        </Cartao>
+      </div>
+      <div className='col-sm-12 col-lg-6 col-xxl-4'>
+        <Cartao cabecalho="23/06/2024">
+        <Pedido 
+            titulo="Memória"
+            descricao="Memória 16Gb"
+            icone="fa-solid fa-memory fa-2x" />
+        </Cartao>
+      </div>
+      <div className='col-sm-12 col-lg-12 col-xxl-4'>
+        <Cartao cabecalho="22/01/2024">
+          <Pedido 
+              titulo="Foguete"
+              descricao="Foguete de verdade"
+              icone="fa-solid fa-shuttle-space fa-2x" /> 
+        </Cartao>
+      </div>      
+    </div>
+    
+  </div>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
